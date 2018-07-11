b5a62e0db0eaeb8fc06878419d560b2d570cd8eb
Boasbabs <babalolasimeon@gmail.com>
2018-07-11 19:12:30 +0000

    Dynamic input field

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo2.png';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: "August 12, 2018",
+            newDeadline: ""
+        }
+    }
+
+    changeDeadline() {
+        this.setState({
+            deadline: this.state.newDeadline,
+        })
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Countdown to 2018 EIT's Graduation</h1>
+                    <h3>Graduation Date: {this.state.deadline}</h3>
+                </header>
+                <div className="App-intro">
+                    <div className="Countdown-meter">
+                        <div>31 days</div>
+                        <div>15 hours</div>
+                        <div>24 minutes</div>
+                        <div>24 seconds</div>
+                    </div>
+                    <div className="Countdown-input">
+                        <input
+                            type="text"
+                            placeholder={'enter date'}
+                            onChange={(event) => this.setState({newDeadline: event.target.value})}
+                        />
+                        <button onClick={() => this.changeDeadline()}>Submit</button>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
